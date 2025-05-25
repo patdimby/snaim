2ef9e4d871d3d5aa8bb0bc9fdfdfffa659a82910
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-05-25 14:57:49 +0300

    add all pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from .views import home
+from .views import home, about, contact, donate, team, testimonial, service, causes, get404
 
 urlpatterns = [
-    path('', home ),
+    path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('donate', donate, name='donate'),
+    path('team', team, name='team'),
+    path('service', service, name='service'),
+    path('testimonial', testimonial, name='testimonial'),
+    path('causes', causes, name='causes'),
+    path('get404', get404, name='get404'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    """" Home page."""
-    return render(request, 'blog/index.html')
+    """" Home page"""
+    return render(request, 'pages/index.html')
+
+def about(request):
+    """" About page"""
+    return render(request, 'pages/about.html')
+
+def causes(request):
+    """" Causes page"""
+    return render(request, 'pages/causes.html')
+
+def team(request):
+    """" Team page"""
+    return render(request, 'pages/team.html')
+
+def service(request):
+    """" Service page"""
+    return render(request, 'pages/service.html')
+
+def testimonial(request):
+    """" Testimonial page"""
+    return render(request, 'pages/testimonial.html')
+
+def donate(request):
+    """" Donate page"""
+    return render(request, 'pages/donate.html')
+
+def contact(request):
+    """" Contact page"""
+    return render(request, 'pages/contact.html')
+
+def get404(request):
+    """" Contact page"""
+    return render(request, 'pages/404.html')
